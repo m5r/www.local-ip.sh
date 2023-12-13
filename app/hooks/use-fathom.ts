4b226b440d37d6ecf1d04f3777259ad8c707b779
m5r <mokht@rmi.al>
2023-12-13 21:48:50 +0100

    add fathom

---
+++ app/hooks/use-fathom.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useLocation } from "@remix-run/react";
+import * as Fathom from "fathom-client";
+
+export default function useFathom() {
+  const location = useLocation();
+
+  useEffect(() => {
+    Fathom.load(window.siteConfig.fathom.siteId, {
+      spa: "history",
+      includedDomains: ["local-ip.sh"],
+    });
+  }, []);
+
+  useEffect(() => {
+    Fathom.trackPageview();
+  }, [location]);
+}
+
--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,61 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import { json, type SerializeFrom, type LinksFunction } from "@remix-run/node";
 import {
-	Links,
-	LiveReload,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
+import useFathom from "./hooks/use-fathom";
+
 export const links: LinksFunction = () => [
-	...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
+export const loader = () =>
+  json({
+    siteConfig: {
+      fathom: {
+        siteId: process.env.FATHOM_SITE_ID!,
+        domain: process.env.FATHOM_CUSTOM_DOMAIN!,
+      },
+    },
+  });
+
+declare global {
+  interface Window {
+    siteConfig: SerializeFrom<typeof loader>["siteConfig"];
+  }
+}
+
 export default function App() {
-	return (
-		<html lang="en">
-		<head>
-			<meta charSet="utf-8" />
-			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<Meta />
-			<Links />
-		</head>
-		<body>
-		<Outlet />
-		<ScrollRestoration />
-		<Scripts />
-		<LiveReload />
-		</body>
-		</html>
-	);
+  const { siteConfig } = useLoaderData<typeof loader>();
+  useFathom();
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <script
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{
+            __html: `window.siteConfig=${JSON.stringify(siteConfig)};`,
+          }}
+        />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
 }
