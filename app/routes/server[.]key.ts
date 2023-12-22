45a61d21b3ac73cc7a5439824f8b2f3319060453
m5r <mokht@rmi.al>
2023-12-22 23:54:06 +0100

    remove unused code

--- app/routes/server[.]key.ts
+++ app/routes/server[.]key.ts
@@ -1,7 +1,3 @@
 export async function loader() {
-	if (process.env.NODE_ENV === "development") {
-		// return fetch("http://localhost:9229/server.key");
-	}
-
 	return fetch("http://local-ip.internal:9229/server.key");
 }
--- app/routes/server[.]pem.ts
+++ app/routes/server[.]pem.ts
@@ -1,7 +1,3 @@
 export async function loader() {
-	if (process.env.NODE_ENV === "development") {
-		// return fetch("http://localhost:9229/server.pem");
-	}
-
 	return fetch("http://local-ip.internal:9229/server.pem");
 }
