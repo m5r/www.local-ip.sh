31f60af66dd2cd79d9e00e2b8769e410401e9eed
m5r <mokht@rmi.al>
2023-12-22 23:56:54 +0100

    add og image

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import { json, type SerializeFrom, type LinksFunction } from "@remix-run/node";
+import { json, type SerializeFrom, type LinksFunction, type MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -11,6 +11,35 @@ import {
 } from "@remix-run/react";
 
 import useFathom from "./hooks/use-fathom";
+
+export const meta: MetaFunction = () => [
+  { name: "title", content: "local-ip.sh" },
+  {
+    name: "description",
+    content: "local-ip.sh is a magic domain name that provides wildcard DNS for any IP address.",
+  },
+  { name: "author", content: "Mokhtar Mial" },
+  { name: "robots", content: "index,follow" },
+  { name: "googlebot", content: "index,follow" },
+  { property: "twitter:title", content: "local-ip.sh" },
+  {
+    property: "twitter:description",
+    content: "local-ip.sh is a magic domain name that provides wildcard DNS for any IP address.",
+  },
+  { property: "twitter:card", content: "summary_large_image" },
+  { property: "twitter:site", content: "https://local-ip.sh/" },
+  { property: "twitter:image", content: "https://local-ip.sh/og.png" },
+  { property: "twitter:image:alt", content: "og image" },
+  { property: "og:title", content: "local-ip.sh" },
+  {
+    property: "og:description",
+    content: "local-ip.sh is a magic domain name that provides wildcard DNS for any IP address.",
+  },
+  { property: "og:url", content: "https://local-ip.sh/" },
+  { property: "og:type", content: "website" },
+  { property: "og:image", content: "https://local-ip.sh/og.png" },
+  { property: "og:image:alt", content: "og image" },
+];
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
